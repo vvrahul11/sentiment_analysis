5cef1001a19285ef493c5394949b7aafed993c40
rahul.veettil <rahul.veettil@orobix.com>
2021-06-13 14:47:56 +0200

    updated init

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,3 @@
 from .rnn_models import RNN, LSTM, LSTM_DROPOUT
-from .train_model import datasplit, gaussiannb, plot_confusion_matrix
+from .train_model import datasplit
+from .ml_models import gaussiannb
