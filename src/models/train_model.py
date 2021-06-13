03ae7ff1087459b91fb68518ffd55e4f7544dcf4
rahul.veettil <rahul.veettil@orobix.com>
2021-06-13 14:47:11 +0200

    added scripts for visualization, analyzing tweets

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -14,7 +14,6 @@ import spacy
 import re
 from scipy.sparse.construct import random
 
-from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.model_selection import train_test_split
 
@@ -23,52 +22,9 @@ import torchtext
 
 from src.features.build_features import bag_of_words
 from src.data.preprocessing import preprocessing, remove_whitespaces
-
-
-
-def datasplit(X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.20,random_state=0)
-    return X_train, X_test, y_train, y_test
-
-
-def gaussiannb(X_train, y_train, X_test):
-     classifier = GaussianNB()
-     classifier.fit(X_train, y_train)
-     y_pred = classifier.predict(X_test)
-
-     return y_pred
-
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    Ref:http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
-    """
-    plt.figure(figsize=(8,6))
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
-
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-
-    plt.tight_layout()
-    plt.ylabel('True label',fontsize=15)
-    plt.xlabel('Predicted label',fontsize=15)
-    plt.show()
+from src.visualization.visualize import plot_confusion_matrix
+from src.data.preprocessing import datasplit
+from src.models.ml_models import gaussiannb
 
 def analyze_restaurant_reviews(path):
     # Read data
@@ -141,13 +97,7 @@ def analyze_tweets(path):
     print(TEXT.vocab.freqs.most_common(50))
 
 
-
-
-
-
-
-
 if __name__ == '__main__':
-    analyze_restaurant_reviews('data/raw/Restaurant_Reviews.tsv')
-    #analyze_tweets('data/external/tweets/tweets.csv')
+    #analyze_restaurant_reviews('data/raw/Restaurant_Reviews.tsv')
+    analyze_tweets('data/external/tweets/tweets.csv')
     #Sentiment Analysis of IMDB reviews
