a943e7d05081e9b0c6850afd4f32b0f4017df7b4
rahul.veettil <rahul.veettil@orobix.com>
2021-06-13 14:45:19 +0200

    updated inits

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,3 +1,3 @@
 from .preprocessing import preprocessing, remove_whitespaces, \
     remove_emojis, remove_digits, remove_mentions_hashtags, \
-        remove_punctuations, remove_punctuations_numbers
+        remove_punctuations, remove_punctuations_numbers, datasplit
